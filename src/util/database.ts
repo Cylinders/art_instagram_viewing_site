8d2e1d20e5862c4a51f07d4d2eb4b761621b2df2
Ruthvik Venkatesan <65773092+Cylinders@users.noreply.github.com>
2024-04-06 09:50:35 -0400

    DATABASE INIT

--- src/util/database.ts
+++ src/util/database.ts
@@ -1,1 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBWxx3OKOEQ1EFfeVruvRtVdFX2z-3czk4",
+  authDomain: "artinstagram-118a5.firebaseapp.com",
+  databaseURL: "https://artinstagram-118a5-default-rtdb.firebaseio.com",
+  projectId: "artinstagram-118a5",
+  storageBucket: "artinstagram-118a5.appspot.com",
+  messagingSenderId: "1071095007421",
+  appId: "1:1071095007421:web:b3bb6d6cc9cef916ebcfd4",
+  measurementId: "G-X52K68T1ZP"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
 export {}
