fa6a30e86d47670c8e222c0d6608582bed364133
Shyiu <mightydragon334@gmail.com>
2024-04-06 12:13:13 -0400

    fixed ruthvik bug

--- src/components/testDB/testDB.tsx
+++ src/components/testDB/testDB.tsx
@@ -4,18 +4,9 @@ import { Link } from "react-router-dom";
 import { useEffect } from "react";
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
-import 
+
 export default function TestDatabase() {
-
-
-
   const TEST = "hello world";
 
-  return <div>
-  {TEST}
-
-
-  </div>;
+  return <div>{TEST}</div>;
 }
-
-
