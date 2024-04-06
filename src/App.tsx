07b80c283b1757937428c5d77e0be4f799c8a257
Ruthvik Venkatesan <65773092+Cylinders@users.noreply.github.com>
2024-04-06 08:35:20 -0400

    init with router DOES NOT WORK

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Page404 from './components/page404/page404';
+// RUTHVIK VENKATESAN: ORIGINAL DRAFT: 12:00 PM Wednesday, March 20, 2024
+import {Route, Routes } from 'react-router-dom';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  // wake me up when its all over
+  // when im wiser and im older
+  return ( <div>
+  
+      <Routes>
+  
+			<Route path="/" element={<MainPage />} />
+			<Route path="/about" element={<About />} />
+			<Route path="/tbt" element={<TbtPage />} />
+			<Route path="/feedback" element={<Feedback />} />
+			<Route path="/blog" element={<Blog />} />
+			<Route path="/contact" element={<Contact />} />
+			<Route path="/fencingcamp" element={<FencingCamp />} />
+			<Route path="*" element={<Page404 />} />
+
+      </Routes>
+
+
+
+      </div>
   );
 }
 
