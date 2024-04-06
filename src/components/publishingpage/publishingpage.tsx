7c26c014fcc9480afb34e73190d9a30be535ab3b
Shyiu <mightydragon334@gmail.com>
2024-04-06 13:16:04 -0400

    pulling changes

--- src/components/publishingpage/publishingpage.tsx
+++ src/components/publishingpage/publishingpage.tsx
@@ -38,6 +38,7 @@ const navigation = [
         </main>
       </div>
     ),
+    id: 0,
   },
   {
     name: "Create New Post",
@@ -56,116 +57,108 @@ const navigation = [
             <form>
               <div className="space-y-12">
                 <div className="border-b border-gray-900/10 pb-12">
-                  <h2 className="text-base font-semibold leading-7 text-gray-900">
-                    New Post
-                  </h2>
-                  <p className="mt-1 text-sm leading-6 text-gray-600">
-                    This information will be displayed publicly so be careful
-                    what you share.
-                  </p>
-
-                  <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                    <div className="sm:col-span-4">
-                      <label
-                        htmlFor="username"
-                        className="block text-sm font-medium leading-6 text-gray-900"
-                      >
-                        Username
-                      </label>
-                      <div className="mt-2">
-                        <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                          <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">
-                            workcation.com/
-                          </span>
-                          <input
-                            type="text"
-                            name="username"
-                            id="username"
-                            autoComplete="username"
-                            className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                            placeholder="janesmith"
-                          />
-                        </div>
-                      </div>
-                    </div>
-
-                    <div className="col-span-full">
-                      <label
-                        htmlFor="about"
-                        className="block text-sm font-medium leading-6 text-gray-900"
-                      >
-                        About
-                      </label>
-                      <div className="mt-2">
-                        <textarea
-                          id="about"
-                          name="about"
-                          rows={3}
-                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                          defaultValue={""}
+                  <div className="sm:col-span-4">
+                    <label
+                      htmlFor="title"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Title
+                    </label>
+                    <div className="mt-2">
+                      <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                        <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">
+                          {" "}
+                        </span>
+                        <input
+                          type="text"
+                          name="username"
+                          id="username"
+                          autoComplete="username"
+                          className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                          placeholder="Title"
                         />
-                      </div>
-                      <p className="mt-3 text-sm leading-6 text-gray-600">
-                        Write a few sentences about yourself.
-                      </p>
-                    </div>
-
-                    <div className="col-span-full">
-                      <label
-                        htmlFor="photo"
-                        className="block text-sm font-medium leading-6 text-gray-900"
-                      >
-                        Photo
-                      </label>
-                      <div className="mt-2 flex items-center gap-x-3">
-                        <UserCircleIcon
-                          className="h-12 w-12 text-gray-300"
-                          aria-hidden="true"
-                        />
-                        <button
-                          type="button"
-                          className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                        >
-                          Change
-                        </button>
-                      </div>
-                    </div>
-
-                    <div className="col-span-full">
-                      <label
-                        htmlFor="cover-photo"
-                        className="block text-sm font-medium leading-6 text-gray-900"
-                      >
-                        Cover photo
-                      </label>
-                      <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
-                        <div className="text-center">
-                          <PhotoIcon
-                            className="mx-auto h-12 w-12 text-gray-300"
-                            aria-hidden="true"
-                          />
-                          <div className="mt-4 flex text-sm leading-6 text-gray-600">
-                            <label
-                              htmlFor="file-upload"
-                              className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
-                            >
-                              <span>Upload a file</span>
-                              <input
-                                id="file-upload"
-                                name="file-upload"
-                                type="file"
-                                className="sr-only"
-                              />
-                            </label>
-                            <p className="pl-1">or drag and drop</p>
-                          </div>
-                          <p className="text-xs leading-5 text-gray-600">
-                            PNG, JPG, GIF up to 10MB
-                          </p>
-                        </div>
                       </div>
                     </div>
                   </div>
+
+                  <div className="col-span-full">
+                    <label
+                      htmlFor="about"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      About
+                    </label>
+                    <div className="mt-2">
+                      <textarea
+                        id="about"
+                        name="about"
+                        rows={3}
+                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        defaultValue={""}
+                      />
+                    </div>
+                    <p className="mt-3 text-sm leading-6 text-gray-600">
+                      Write a few sentences about yourself.
+                    </p>
+                  </div>
+
+                  <div className="col-span-full">
+                    <label
+                      htmlFor="photo"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Photo
+                    </label>
+                    <div className="mt-2 flex items-center gap-x-3">
+                      <UserCircleIcon
+                        className="h-12 w-12 text-gray-300"
+                        aria-hidden="true"
+                      />
+                      <button
+                        type="button"
+                        className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                      >
+                        Change
+                      </button>
+                    </div>
+                  </div>
+
+                  <div className="col-span-full">
+                    <label
+                      htmlFor="cover-photo"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Cover photo
+                    </label>
+                    <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                      <div className="text-center">
+                        <PhotoIcon
+                          className="mx-auto h-12 w-12 text-gray-300"
+                          aria-hidden="true"
+                        />
+                        <div className="mt-4 flex text-sm leading-6 text-gray-600">
+                          <label
+                            htmlFor="file-upload"
+                            className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                          >
+                            <span>Upload a file</span>
+                            <input
+                              id="file-upload"
+                              name="file-upload"
+                              type="file"
+                              className="sr-only"
+                            />
+                          </label>
+                          <p className="pl-1">or drag and drop</p>
+                        </div>
+                        <p className="text-xs leading-5 text-gray-600">
+                          PNG, JPG, GIF up to 10MB
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6"></div>
                 </div>
 
                 <div className="border-b border-gray-900/10 pb-12">
@@ -483,6 +476,7 @@ const navigation = [
         </main>
       </div>
     ),
+    id: 1,
   },
 ];
 const userNavigation = [{ name: "Sign out", href: "#" }];
@@ -495,7 +489,9 @@ function classNames(...classes) {
 export default function PublishingPage() {
   const [value, setValue] = useState(navigation[0].content);
 
-  const handleClick = (buttonValue: any) => {
+  const handleClick = (buttonValue: any, number: number) => {
+    navigation[number].current = true;
+    navigation[(number + 1) % 2].current = false;
     setValue(buttonValue);
   };
 
@@ -527,7 +523,7 @@ export default function PublishingPage() {
                               "rounded-md px-3 py-2 text-sm font-medium"
                             )}
                             aria-current={item.current ? "page" : undefined}
-                            onClick={() => handleClick(item.content)}
+                            onClick={() => handleClick(item.content, item.id)}
                           >
                             {item.name}
                           </button>
