76f786ced9be8131a80ddf3001ec06f517edd297
Shyiu <mightydragon334@gmail.com>
2024-04-06 15:03:08 -0400

    submit function works

--- src/components/publishingpage/publishingpage.tsx
+++ src/components/publishingpage/publishingpage.tsx
@@ -18,163 +18,6 @@ const user = {
     "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
 };
 
-const navigation = [
-  {
-    name: "Current Posts",
-    current: true,
-    content: (
-      <div>
-        <header className="bg-white shadow">
-          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold tracking-tight text-gray-900">
-              Current Posts
-            </h1>
-          </div>
-        </header>
-        <main>
-          <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-            hello world
-          </div>
-        </main>
-      </div>
-    ),
-    id: 0,
-  },
-  {
-    name: "Create New Post",
-    current: false,
-    content: (
-      <div>
-        <header className="bg-white shadow">
-          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold tracking-tight text-gray-900">
-              Create New Post
-            </h1>
-          </div>
-        </header>
-        <main>
-          <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-            <form>
-              <div className="space-y-12">
-                <div className="border-b border-gray-900/10 pb-12">
-                  <div className="sm:col-span-4">
-                    <label
-                      htmlFor="title"
-                      className="block text-sm font-medium leading-6 text-gray-900"
-                    >
-                      Title
-                    </label>
-                    <div className="mt-2">
-                      <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                        <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">
-                          {" "}
-                        </span>
-                        <input
-                          type="text"
-                          name="title"
-                          id="title"
-                          className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                          placeholder="Title"
-                        />
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="col-span-full">
-                    <label
-                      htmlFor="cover-photo"
-                      className="block text-sm font-medium leading-6 text-gray-900"
-                    >
-                      Cover photo
-                    </label>
-                    <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
-                      <div className="text-center">
-                        <PhotoIcon
-                          className="mx-auto h-12 w-12 text-gray-300"
-                          aria-hidden="true"
-                        />
-                        <div className="mt-4 flex text-sm leading-6 text-gray-600">
-                          <label
-                            htmlFor="file-upload"
-                            className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
-                          >
-                            <span>Upload a file</span>
-                            <input
-                              id="file-upload"
-                              name="file-upload"
-                              type="file"
-                              className="sr-only"
-                            />
-                          </label>
-                          <p className="pl-1">or drag and drop</p>
-                        </div>
-                        <p className="text-xs leading-5 text-gray-600">
-                          PNG, JPG, GIF up to 10MB
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="col-span-full">
-                    <label
-                      htmlFor="description"
-                      className="block text-sm font-medium leading-6 text-gray-900"
-                    >
-                      Description
-                    </label>
-                    <div className="mt-2">
-                      <textarea
-                        id="about"
-                        name="about"
-                        rows={3}
-                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        defaultValue={""}
-                        placeholder={"Write a few sentences about your post."}
-                      />
-                    </div>
-                  </div>
-
-                  <div className="sm:col-span-4">
-                    <label
-                      htmlFor="tags"
-                      className="block text-sm font-medium leading-6 text-gray-900"
-                    >
-                      Tags
-                    </label>
-                    <div className="mt-2">
-                      <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                        <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">
-                          {" "}
-                        </span>
-                        <input
-                          type="text"
-                          name="tags"
-                          id="tags"
-                          className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                          placeholder="Seperate Tags with spaces i.e. watercolor portrait"
-                        />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div className="mt-6 flex items-center justify-end gap-x-6">
-                <button
-                  type="submit"
-                  className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Submit
-                </button>
-              </div>
-            </form>
-          </div>
-        </main>
-      </div>
-    ),
-    id: 1,
-  },
-];
 const userNavigation = [{ name: "Sign out", href: "#" }];
 
 // @ts-ignore
@@ -183,6 +26,199 @@ function classNames(...classes) {
 }
 
 export default function PublishingPage() {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [selectedTitle, setSelectedTitle] = useState(null);
+  const [selectedDescription, setSelectedDescription] = useState(null);
+  const [selectedTags, setSelectedTags] = useState(null);
+
+  const handleFileInputChange = (event: any) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const handleTitleChange = (event: any) => {
+    setSelectedTitle(event.target.value);
+  };
+
+  const handleDescriptionChange = (event: any) => {
+    setSelectedDescription(event.target.value);
+  };
+
+  const handleTagsChange = (event: any) => {
+    setSelectedTags(event.target.value);
+  };
+
+  const handleSubmit = (event: all) => {
+    event.preventDefault();
+
+    console.log("Title:", selectedTitle);
+    console.log("Description:", selectedDescription);
+    console.log("Tags:", selectedTags);
+    console.log("File", selectedFile);
+  };
+
+  const navigation = [
+    {
+      name: "Current Posts",
+      current: true,
+      content: (
+        <div>
+          <header className="bg-white shadow">
+            <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+              <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+                Current Posts
+              </h1>
+            </div>
+          </header>
+          <main>
+            <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+              hello world
+            </div>
+          </main>
+        </div>
+      ),
+      id: 0,
+    },
+    {
+      name: "Create New Post",
+      current: false,
+      content: (
+        <div>
+          <header className="bg-white shadow">
+            <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+              <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+                Create New Post
+              </h1>
+            </div>
+          </header>
+          <main>
+            <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+              <form onSubmit={handleSubmit}>
+                <div className="space-y-12">
+                  <div className="border-b border-gray-900/10 pb-12">
+                    <div className="sm:col-span-4">
+                      <label
+                        htmlFor="title"
+                        className="block text-sm font-medium leading-6 text-gray-900"
+                      >
+                        Title
+                      </label>
+                      <div className="mt-2">
+                        <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                          <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">
+                            {" "}
+                          </span>
+                          <input
+                            type="text"
+                            name="title"
+                            id="title"
+                            className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                            placeholder="Title"
+                            onChange={handleTitleChange}
+                          />
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="col-span-full">
+                      <label
+                        htmlFor="cover-photo"
+                        className="block text-sm font-medium leading-6 text-gray-900"
+                      >
+                        Cover photo
+                      </label>
+                      <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                        <div className="text-center">
+                          <PhotoIcon
+                            className="mx-auto h-12 w-12 text-gray-300"
+                            aria-hidden="true"
+                          />
+                          <div className="mt-4 flex text-sm leading-6 text-gray-600">
+                            <label
+                              htmlFor="file-upload"
+                              className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                            >
+                              <span>Upload a file</span>
+                              <input
+                                id="file-upload"
+                                name="file-upload"
+                                type="file"
+                                className="sr-only"
+                                onChange={handleFileInputChange}
+                              />
+                            </label>
+                            <p className="pl-1">or drag and drop</p>
+                          </div>
+                          <p className="text-xs leading-5 text-gray-600">
+                            PNG, JPG, GIF up to 10MB
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="col-span-full">
+                      <label
+                        htmlFor="description"
+                        className="block text-sm font-medium leading-6 text-gray-900"
+                      >
+                        Description
+                      </label>
+                      <div className="mt-2">
+                        <textarea
+                          id="about"
+                          name="about"
+                          rows={3}
+                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                          defaultValue={""}
+                          placeholder={"Write a few sentences about your post."}
+                          onChange={handleDescriptionChange}
+                        />
+                      </div>
+                    </div>
+
+                    <div className="sm:col-span-4">
+                      <label
+                        htmlFor="tags"
+                        className="block text-sm font-medium leading-6 text-gray-900"
+                      >
+                        Tags
+                      </label>
+                      <div className="mt-2">
+                        <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                          <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">
+                            {" "}
+                          </span>
+                          <input
+                            type="text"
+                            name="tags"
+                            id="tags"
+                            className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                            placeholder="Seperate Tags with spaces i.e. watercolor portrait"
+                            onChange={handleTagsChange}
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="mt-6 flex items-center justify-end gap-x-6">
+                  <button
+                    type="submit"
+                    className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    onClick={handleSubmit}
+                  >
+                    Submit
+                  </button>
+                </div>
+              </form>
+            </div>
+          </main>
+        </div>
+      ),
+      id: 1,
+    },
+  ];
+
   const [value, setValue] = useState(navigation[0].content);
 
   const handleClick = (buttonValue: any, number: number) => {
