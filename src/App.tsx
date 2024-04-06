a249099211f44fdabe8b46c5b6854ba9a480f738
Ruthvik Venkatesan <65773092+Cylinders@users.noreply.github.com>
2024-04-06 09:05:16 -0400

    PUSH

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Page404 from './components/page404/page404';
-// RUTHVIK VENKATESAN: ORIGINAL DRAFT: 12:00 PM Wednesday, March 20, 2024
+import MainPage from './components/mainpage/mainpage';
+
 import {Route, Routes } from 'react-router-dom';
 function App() {
   // wake me up when its all over
@@ -12,14 +12,6 @@ function App() {
       <Routes>
   
 			<Route path="/" element={<MainPage />} />
-			<Route path="/about" element={<About />} />
-			<Route path="/tbt" element={<TbtPage />} />
-			<Route path="/feedback" element={<Feedback />} />
-			<Route path="/blog" element={<Blog />} />
-			<Route path="/contact" element={<Contact />} />
-			<Route path="/fencingcamp" element={<FencingCamp />} />
-			<Route path="*" element={<Page404 />} />
-
       </Routes>
 
 
---
+++ src/components/mainpage/mainpage.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom'
+import { useEffect } from 'react';
+import { NavLink } from "react-router-dom";
+
+
+export default function MainPage() {
+
+	useEffect(() => {
+		document.title = "RedPanda Fencing"
+        window.scrollTo(0,0)
+	}, []);
+
+
+	return <div>
+			
+			TEST TEST
+        
+		</div>
+
+
+
+
+
+
+}
